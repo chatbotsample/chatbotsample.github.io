6f6faaaf3002f5d787f9d1c97fa7a5654b90d781
diskii <diskii100@gmail.com>
2021-04-04 02:17:31 +0900

    deploying latest code to develop

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo 'Script for deploying code to master'
+npm run build
+echo 'Dist file building is now complete'
+# deplying to gh pages
+# when script doesnt work
+# git push origin `git subtree split --prefix dist master`:gh-pages --force
+while true; do
+    read -p "Would you like to deploy the code to the master branch:  `echo $'\n> '`" yn
+    case $yn in
+        [Yy]* )
+          git add -A
+          git commit -m 'deploying latest code to develop'
+          git push
+          git subtree push --prefix dist origin master;
+          break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
